81151466da19f2bbbf0889591df0813906aa0b0d
Adrien <adrienjl.rousseau@gmail.com>
2016-10-31 20:31:50 +0100

    improved capture

--- capture_chat.py
+++ capture_chat.py
@@ -7,8 +7,8 @@ def chat(sock, msg):
 	msg  -- the message to be sent
 	"""
 	print "Trying to send"
-	sock.send("PRIVMSG #{} :{} \r\n".format(cfg.CHAN, msg))
-	print "PRIVMSG #{} :{}".format(cfg.CHAN, msg)
+	sock.send("PRIVMSG #{} :{} \r\n".format(chan, msg))
+	print "PRIVMSG #{} :{}".format(chan, msg)
 
 
 import cfg
@@ -17,16 +17,21 @@ import re
 import time
 import datetime
 import math
+import sys 
+global chan
+
 RATE = (20/30) # messages per second
 
 # network functions go here
 
+#chan = cfg.CHAN
+chan = sys.argv[1]
 
 s = socket.socket()
 s.connect((cfg.HOST, cfg.PORT))
 s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
 s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
-s.send("JOIN #{}\r\n".format(cfg.CHAN).encode("utf-8"))
+s.send("JOIN #{}\r\n".format(chan).encode("utf-8"))
 
 CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 
@@ -35,13 +40,14 @@ CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
 now = datetime.datetime.now()
 stamp = [str(x) for x in [now.day,now.month,now.hour,now.minute]]
 
-file_name =cfg.CHAN + '_' + "_".join(stamp)
+file_name =chan + '_' + "_".join(stamp)
 
-id2 = "PRIVMSG #"+cfg.CHAN +" :"
+id2 = "PRIVMSG #"+chan +" :"
+time_till_last_msg = 0
 
 with open(file_name+".csv",'w') as f, open(file_name+'_error' +".csv",'w') as fe:
 	while True:
-		response = s.recv(1024).decode("utf-8")
+		response = s.recv(4096).decode("utf-8")
 		if response == "PING :tmi.twitch.tv\r\n":
 			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
 		else:
@@ -63,4 +69,4 @@ with open(file_name+".csv",'w') as f, open(file_name+'_error' +".csv",'w') as fe
 						print "Error" + line
 						fe.write(str(math.floor(time.time())).encode("utf-8") + ";" + line.encode("utf-8") + '\n')
 						pass
-		time.sleep(0.1)
+		time.sleep(0.05)
