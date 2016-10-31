be3afc3dc9d575ff398c149c492a4d429bc147ea
Adrien <adrienjl.rousseau@gmail.com>
2016-10-31 14:20:40 +0100

    first commit

---
+++ bot.py
@@ -0,0 +1,66 @@
+
+def chat(sock, msg):
+	"""
+	Send a chat message to the server.
+	Keyword arguments:
+	sock -- the socket over which to send the message
+	msg  -- the message to be sent
+	"""
+	print "Trying to send"
+	sock.send("PRIVMSG #{} :{} \r\n".format(cfg.CHAN, msg))
+	print "PRIVMSG #{} :{}".format(cfg.CHAN, msg)
+
+
+import cfg
+import socket
+import re
+import time
+import datetime
+import math
+RATE = (20/30) # messages per second
+
+# network functions go here
+
+
+s = socket.socket()
+s.connect((cfg.HOST, cfg.PORT))
+s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
+s.send("JOIN #{}\r\n".format(cfg.CHAN).encode("utf-8"))
+
+CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+#chat(s,"FeelsGoodMan")
+
+now = datetime.datetime.now()
+stamp = [str(x) for x in [now.day,now.month,now.hour,now.minute]]
+
+file_name =cfg.CHAN + '_' + "_".join(stamp)
+
+id2 = "PRIVMSG #"+cfg.CHAN +" :"
+
+with open(file_name+".csv",'w') as f, open(file_name+'_error' +".csv",'w') as fe:
+	while True:
+		response = s.recv(1024).decode("utf-8")
+		if response == "PING :tmi.twitch.tv\r\n":
+			s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+		else:
+			if response == None:
+				pass
+			else:
+				for line in [ x for x in response.split('\n') if x != '']:
+					try:
+						username = line[1:line.index('!')]
+						message = line[line.index(id2)+len(id2):]
+						if username !='idenoca' and username != 'tmi':
+							f.write(username+";"+str(int(math.floor(time.time()))) + ";" + message)
+						print(username + ": " + message)
+						for pattern in cfg.PATT:
+							if re.match(pattern, message):
+								print "LUL"
+								break
+					except:
+						print "Error" + line
+						fe.write(str(math.floor(time.time())).encode("utf-8") + ";" + line.encode("utf-8") + '\n')
+						pass
+		time.sleep(0.1)
