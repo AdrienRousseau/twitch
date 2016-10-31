8cbba7d8f63eeb02e1e78896c214d982b46a998e
Adrien <adrienjl.rousseau@gmail.com>
2016-10-31 20:31:33 +0100

    added checking alive

---
+++ checking_alive.py
@@ -0,0 +1,73 @@
+from urllib.request import urlopen
+import urllib.request
+from urllib.error import URLError
+import urllib.parse
+from threading import Timer
+import os
+import time
+import json
+import sys
+import subprocess
+import datetime
+import cfg
+ 
+def check_user(user):
+	""" returns 0: online, 1: offline, 2: not found, 3: error """
+	global info
+	url = 'https://api.twitch.tv/kraken/streams/' + user
+	request = urllib.request.Request(url, headers={"Client-ID" : cfg.CLIENTID})
+	try:
+		info = json.loads(urlopen(request).read().decode('utf-8'))
+		if info['stream'] == None:
+			status = 1
+		else:
+			status = 0
+	except URLError as e:
+		if e.reason == 'Not Found' or e.reason == 'Unprocessable Entity':
+			status = 2
+		else:
+			status = 3
+	return status
+
+
+def loopcheck():
+	while True:
+		status = check_user(user)
+		if status == 2:
+			print("username not found. invalid username?")
+		elif status == 3:
+			print(datetime.datetime.now().strftime("%Hh%Mm%Ss")," ","unexpected error. will try again in 5 minutes.")
+			time.sleep(300)
+		elif status == 1:
+			print(user,"currently offline, checking again in",refresh,"seconds")
+			time.sleep(refresh) # 30 seconds
+		elif status == 0:
+			print(user,"online. stop.")
+			filename = user+" - "+datetime.datetime.now().strftime("%Y-%m-%d %Hh%Mm%Ss")+" - "+(info['stream']).get("channel").get("status")+".mp4"
+			filename = format_filename(filename)
+			#subprocess.call(["livestreamer", "twitch.tv/"+user,quality,"-o",directory+filename])
+			subprocess.call(["python","bot.py"])
+			print("Stream is done. Going back to checking..")
+			time.sleep(15)
+ 
+def main():
+	global refresh
+	global user
+	global quality
+	global directory
+	global post_args
+	refresh = 30.0
+	user = sys.argv[1]
+	client = False
+
+
+	if(refresh<15):
+		print("Check interval should not be lower than 15 seconds")
+		refresh=15
+
+	print("Checking for",user,"every",refresh,"seconds.")
+	loopcheck()
+
+if __name__ == "__main__":
+	# execute only if run as a script
+	main()
